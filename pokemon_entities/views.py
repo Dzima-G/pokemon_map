423403faedc62ae32622fadf0bc1a7fb524cb2e0
Dzima-G <dgukalin@gmail.com>
2023-08-15 19:55:18 +0300

    added pokemons display by specified parameters

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from pokemon_entities.models import Pokemon, PokemonEntity
+from django.utils import timezone
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -27,16 +28,16 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
+    local_time = timezone.localtime(timezone.now())
+    pokemons_entity = PokemonEntity.objects.all()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
+    for pokemon_entity in pokemons_entity:
+        if pokemon_entity.appeared_at <= local_time <= pokemon_entity.disappeared_at:
             add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
+                folium_map,
+                pokemon_entity.lat,
+                pokemon_entity.lon,
+                image_url=request.build_absolute_uri(pokemon_entity.pokemon.image.url)
             )
 
     pokemons = Pokemon.objects.all()
