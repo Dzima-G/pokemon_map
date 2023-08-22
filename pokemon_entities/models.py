6354a69e08041e8904ef060ba3bd2cfa9a8ff4f0
Dzima-G <dgukalin@gmail.com>
2023-08-22 19:10:26 +0300

    fixed type of fields in the model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200, default='Заголовок', verbose_name='Название на русском')
-    title_en = models.TextField(max_length=200, default='title', verbose_name='Название на английском')
-    title_jp = models.TextField(max_length=200, default='タイトル', verbose_name='Название на японском')
+    title_en = models.CharField(max_length=200, default='title', verbose_name='Название на английском')
+    title_jp = models.CharField(max_length=200, default='タイトル', verbose_name='Название на японском')
     image = models.ImageField(upload_to='pokemon', null=True, verbose_name='Изображение покемона')
     description = models.TextField(max_length=500, default="Описание", verbose_name='Описание покемона')
     evolution = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='from_evolved',
